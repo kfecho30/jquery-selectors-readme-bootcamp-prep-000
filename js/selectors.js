8247f2bbfc76403ed4a92abda949db8e2194757c
Kelsey Fecho <kfecho30@gmail.com>
2018-02-28 01:41:40 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -3,6 +3,10 @@ function paragraphSelector(){
   return $('p');
 }
 
-function lastImageSelector){
+function lastImageSelector(){
   return $('img:last');
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja');
 }
